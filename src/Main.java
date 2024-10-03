b5b71427e32e942c0361b0a0e0847911ab052581
edducon <e@panpushnyy.ru>
2024-10-03 17:44:21 +0300

    03.10 - 16:10 Лекция

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,31 @@
-import java.util.Scanner;
+static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    Date now = new Date();
 
-import static java.lang.Math.*;
+    System.out.println(now);
+    System.out.println(now.getYear() + 1900);
 
-class Main {
+    /*System.out.println("Введите число x: ");
+    int x = in.nextInt();
 
-    static double y(
-            double x){
-        double res = 2 * x +3;
-        return res;
+    if (x < 10) {
+        System.out.println();
+    } else if (x == 30) {
+        System.out.println();
+    } else {
+        System.out.println();
     }
+}
 
-    static double areaDisk(double r){
-        return PI*pow(r, 2);
-    }
-
-    static void main(String[] args) {
-
-//        int [] arr = new int [] {2,5,3};
-//        System.out.println(arr.length);
-//        System.out.println(arr[1]);
-//        int [] arr2 = arr;
-//        arr2[1] = 100;
-//        System.out.println(arr2[1]);
-//        System.out.println(arr[1]);
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите радиус круга");
-        double r = in.nextDouble();
-        double s = areaDisk(r);
-        int y = 10;
-        System.out.println(y(10.0) + y(10.0));
-
-        System.out.println("s=" + (int)(s * 100)/100.00);
+    if (x == 0) System.out.println("012345");
+    if (x%2 == 0) System.out.println("Четное число");
+    if (x%2 != 0) System.out.println("Нечетное число");*/
 
 
-        System.out.println((int)(random() * 3 + 3));
+
+    /*if (x > 10 & x%2 == 0) {
+        System.out.println(x);
+    }*/
 
 
-//        int a = 3;
-//        int b = 5;
-//        System.out.println(a/b);
-//        System.out.println((double)a/b);
-
-    }
-    
 }
